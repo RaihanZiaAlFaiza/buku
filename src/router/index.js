1d9f666bf32d2227dc0459df2460c247d545c222
Raihanzia <raihanziaalfaiza@gmail.com>
2023-06-28 21:05:52 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Books from "../views/Books.vue";
+import Categories from "../views/Categories.vue";
+import Member from "@/views/Member.vue";
+import Desc from "@/views/Information.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/books",
+      name: "Books",
+      component: Books,
+    },
+    {
+      path: "/categories",
+      name: "Categories",
+      component: Categories,
+    },
+    {
+      path: "/member",
+      name: "Member",
+      component: Member,
+    },
+    {
+      path: "/desc",
+      name: "Desc",
+      component: Desc,
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,39 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+      <img src="../assets/welcomebg.png" class="img-fluid" />
+      <h1 class="text-white">TO REY BOOK STORE!</h1>
+    </div>
+    <div class="col-lg-4">
+      <img src="../assets/bookbg.png" class="img-fluid" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+  },
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.banner-image {
+  background: #138fc8;
+  font-family: "Times New Roman";
+  font-style: italic;
+  font-size: 30px;
+}
+.col-lg-4 {
+  margin-top: 40px;
+  margin-left: 20px;
+}
+.col-lg-5 {
+  margin-left: -400px;
+  margin-bottom: 500px;
+}
+</style>
